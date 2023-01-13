07249e08820389a6dc66646212289f3bf86a27a2
sunilsch <120412820+sunilsch@users.noreply.github.com>
2023-01-13 11:19:07 +0100

    Change password rules

--- src/main/java/com/sunils/MusicPlayerBackend/Controller/UserController.java
+++ src/main/java/com/sunils/MusicPlayerBackend/Controller/UserController.java
@@ -3,12 +3,14 @@ package com.sunils.MusicPlayerBackend.Controller;
 import com.sunils.MusicPlayerBackend.Entities.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.sql.DataSource;
 
@@ -21,9 +23,13 @@ public class UserController {
 
     @PostMapping(path="/newUser", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public UserDetails newUser(@RequestBody UserEntity body) {
+        String password = new BCryptPasswordEncoder().encode(body.getPassword());
+        if(password.length() < 8){
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE,"Password must have a length of minimum 8");
+        }
         UserDetails user = User.builder()
                 .username(body.getUsername())
-                .password(new BCryptPasswordEncoder().encode(body.getPassword()))
+                .password(password)
                 .roles(body.getRole())
                 .build();
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
